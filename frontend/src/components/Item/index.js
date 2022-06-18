c5fccca8e35b94f3a34d590f5682139f363e5410
Ido Ziv <zivido@gmail.com>
2022-06-19 00:16:17 +0300

    Fixing #INC-5312: Missing image

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -50,8 +50,12 @@ class Item extends React.Component {
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image}
-                alt={this.props.item.title}
+                src={
+                  this.props.item.image
+                    ? this.props.item.image
+                    : "../placeholder.png"
+                }
+                alt={"this.props.item.title" + Date.now()}
                 className="item-img"
                 style={{ height: "500px", width: "100%", borderRadius: "6px" }}
               />
