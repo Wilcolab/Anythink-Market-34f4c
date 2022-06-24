c13f1f6dedff97a6869d4c2feac89f2dac6fe778
Ido Ziv <zivido@gmail.com>
2022-06-24 21:46:24 +0300

    Added search bar

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,14 +1,44 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
 
-const Banner = () => {
+const Banner = (props) => {
+  function handleChange(event) {
+    let length = event.target.value.length;
+
+    if (length >= 3) {
+      props.onSearch(
+        event.target.value,
+        (page) => agent.Items.byTitle(event.target.value, page),
+        agent.Items.byTitle(event.target.value)
+      );
+    } else {
+      const tab = "all";
+      const itemsPromise = agent.Items.all;
+      props.onLoad(
+        tab,
+        itemsPromise,
+        Promise.all([agent.Tags.getAll(), itemsPromise()])
+      );
+    }
+  }
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <span> the cool stuff.</span>
+          <span id="get-part" style={{ paddingRight: "10px" }}>
+            A place to get
+          </span>
+          <span>
+            <input
+              className="search-bar"
+              id="search-box"
+              placeholder="What is it that you truly desire"
+              onChange={handleChange}
+            />
+          </span>
+          <span style={{ paddingLeft: "10px" }}> the cool sfuff</span>
         </div>
       </div>
     </div>
