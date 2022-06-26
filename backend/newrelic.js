092397a7fa280f2a1bdf3613cd81db4c7a9c5496
Ido Ziv <zivido@gmail.com>
2022-06-26 17:18:24 +0300

    fix

--- backend/newrelic.js
+++ backend/newrelic.js
@@ -6,14 +6,6 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
-  /**
-   * Array of application names.
-   */
-  // app_name: app_name,
-  /**
-   * Your New Relic license key.
-   */
-  // license_key: license_key,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
